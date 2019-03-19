046bee437f93ad509f86cfb392d7053f2af85b86
Danilo S Almeida <dsalinux@gmail.com>
2019-03-19 12:02:37 -0300

    Criação da classe FabricaDeConexao

---
+++ sistema-comercial-tads/src/main/java/br/edu/ifg/sistemacomercial/util/FabricadeConexao.java
@@ -0,0 +1,30 @@
+package br.edu.ifg.sistemacomercial.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricadeConexao {
+
+    private static Connection conexao;
+    private static final String URL = "jdbc:postgresql://localhost/siscom";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "root";
+    
+    
+    public static Connection getConexao() throws SQLException{
+        if(conexao == null){
+            conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+        }
+        return conexao;
+    }
+    
+    public static void fecharConexao() throws SQLException{
+        if(conexao != null){
+            conexao.close();
+        } 
+        conexao = null;
+    }
+    
+    
+}
