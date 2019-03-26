3d1548521ad1518a82f97c91ceeef95d5e14f208
Danilo Souza Almeida <dsalinux@gmail.com>
2019-03-26 07:50:24 -0300

    Corrigindo problema no SQL

--- sistema-comercial-tads/src/main/webapp/META-INF/script.sql
+++ sistema-comercial-tads/src/main/webapp/META-INF/script.sql
@@ -26,7 +26,7 @@ CREATE TABLE public.usuario
     senha character varying(32) COLLATE pg_catalog."default" NOT NULL,
     nome character varying(80) COLLATE pg_catalog."default" NOT NULL,
     login character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    id integer NOT NULL DEFAULT nextval('usuario_id_seq'::regclass),
+    id serial NOT NULL,
     email character varying(1200) COLLATE pg_catalog."default" NOT NULL
 )
 WITH (
