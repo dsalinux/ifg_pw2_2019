90026cde146b4db9f09a0de69d928db2ca3fb3bc
Danilo Souza Almeida <dsalinux@gmail.com>
2019-05-14 09:53:41 -0300

    Implementação do novo modelo de Dados

--- sistema-comercial-tads/src/main/java/br/edu/ifg/sistemacomercial/util/PersistenceFactory.java
+++ sistema-comercial-tads/src/main/java/br/edu/ifg/sistemacomercial/util/PersistenceFactory.java
@@ -4,6 +4,8 @@ import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
 
 public class PersistenceFactory {
 
@@ -14,7 +16,14 @@ public class PersistenceFactory {
         entityManager = Persistence
                 .createEntityManagerFactory("sistema_comercial_tads_PU")
                 .createEntityManager();
+
         
+        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:5432/siscom", "postgres","ifg").load();
+//        Flyway flyway = Flyway.configure().dataSource(
+//                entityManager.getEntityManagerFactory().getProperties().get("javax.persistence.jdbc.url").toString(), 
+//                entityManager.getEntityManagerFactory().getProperties().get("javax.persistence.jdbc.user").toString(),
+//                entityManager.getEntityManagerFactory().getProperties().get("javax.persistence.jdbc.password").toString()).load();
+        flyway.migrate();
         return entityManager;
     }
     
---
+++ sistema-comercial-tads/src/main/resources/db/migration/V1__CRIACAO_BANCO.sql
@@ -0,0 +1,135 @@
+CREATE TABLE public.categoria (
+    id serial NOT NULL,
+    nome varchar(40) NOT NULL,
+    categoria_id integer NOT NULL references public.categoria(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.usuario (
+    id SERIAL NOT NULL,
+    nome varchar(60) NOT NULL,
+    email varchar(100) NOT NULL,
+    senha varchar(100) NOT NULL,
+    data_cadastro date NOT NULL,
+    data_desativacao timestamp,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.permissao (
+    id Serial NOT NULL,
+    nome varchar(50) NOT NULL,
+    descricao varchar(100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.usuario_has_permissao (
+    usuario_id integer NOT NULL references public.usuario(id),
+    permissao_id integer NOT NULL references public.permissao(id),
+    PRIMARY KEY (usuario_id, permissao_id)
+);
+
+CREATE TABLE public.cliente (
+    id serial NOT NULL,
+    nome varchar NOT NULL,
+    apelido_fantasia varchar,
+    cpf varchar,
+    rg varchar,
+    cnpj varchar,
+    inscricao varchar,
+    telefone1 varchar NOT NULL,
+    telefone2 varchar NOT NULL,
+    telefone3 varchar NOT NULL,
+    email varchar NOT NULL,
+    observacao varchar NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.endereco (
+    id serial NOT NULL,
+    logradouro varchar NOT NULL,
+    numero varchar NOT NULL,
+    complemento varchar NOT NULL,
+    bairro varchar NOT NULL,
+    cidade varchar NOT NULL,
+    pais varchar NOT NULL,
+    cep varchar NOT NULL,
+    cliente_id integer NOT NULL,
+    cobranca boolean NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.produto (
+    id serial NOT NULL,
+    nome varchar(100) NOT NULL,
+    marca varchar(50),
+    categoria_id integer NOT NULL references public.categoria(id),
+    unidade varchar(3) NOT NULL,
+    codigo_barras varchar(100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.fornecedor (
+    id serial NOT NULL,
+    nome varchar(100) NOT NULL,
+    cnpj varchar(20),
+    cpf varchar(20),
+    inscricao varchar(20),
+    rg varchar(20),
+    telefone1 varchar(20) NOT NULL,
+    telefone2 varchar(20) NOT NULL,
+    email varchar(120) NOT NULL,
+    website varchar(150) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.movimento_estoque (
+    id serial NOT NULL,
+    produto_id integer NOT NULL references public.produto(id),
+    fornecedor_id integer NOT NULL references public.fornecedor(id),
+    tipo_movimento varchar(10) NOT NULL,
+    data_movimento timestamp NOT NULL,
+    quantidade_recebida decimal NOT NULL,
+    preco_custo decimal(10,2) NOT NULL,
+    observacao varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE public.pedido (
+    id serial NOT NULL,
+    cliente_id integer NOT NULL references public.cliente(id),
+    data_pedido date NOT NULL,
+    observacoes varchar(150) NOT NULL,
+    observacoes_cliente varchar(150) NOT NULL,
+    usuario_id integer NOT NULL references public.usuario(id),
+    desconto decimal(10,2) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE public.pedido_has_produto (
+    produto_id integer NOT NULL references public.produto(id),
+    pedido_id integer NOT NULL references public.pedido(id),
+    valor decimal(10,1) NOT NULL,
+    quantidade decimal(10,2) NOT NULL,
+    PRIMARY KEY (produto_id, pedido_id)
+);
+
+CREATE TABLE public.conta (
+    id serial NOT NULL,
+    nome varchar(80) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.fluxo_caixa (
+    id Serial NOT NULL,
+    descricao varchar(100) NOT NULL,
+    tipo_movimento varchar(20) NOT NULL,
+    pedido_id integer references public.pedido(id),
+    valor decimal(10,2) NOT NULL,
+    data_movimento timestamp NOT NULL,
+    conta_id integer NOT NULL references public.conta(id),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO public.usuario(nome, email, senha, data_cadastro, data_desativacao)	VALUES ('IFG', 'ifg@ifg.edu.br', '123', '2019-05-14', null);
