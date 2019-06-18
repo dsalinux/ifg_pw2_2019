4255aa707508773c9f7cf37c1d8b578cbe8ee13a
Lucas Alves <LucasAlves16.la@gmail.com>
2019-06-18 11:29:01 -0300

    Validação de dados do Fornecedor

--- sistema-comercial-tads/src/main/java/br/edu/ifg/sistemacomercial/logic/FornecedorLogic.java
+++ sistema-comercial-tads/src/main/java/br/edu/ifg/sistemacomercial/logic/FornecedorLogic.java
@@ -2,6 +2,7 @@ package br.edu.ifg.sistemacomercial.logic;
 
 import br.edu.ifg.sistemacomercial.dao.FornecedorDAO;
 import br.edu.ifg.sistemacomercial.entity.Fornecedor;
+import br.edu.ifg.sistemacomercial.util.Assert;
 import br.edu.ifg.sistemacomercial.util.exception.NegocioException;
 import br.edu.ifg.sistemacomercial.util.exception.SistemaException;
 import java.util.List;
@@ -17,6 +18,16 @@ public class FornecedorLogic implements GenericLogic<Fornecedor, Integer> {
         if("".equals(entity.getNome().trim())){
             throw new NegocioException("Nome do fornecedor é obrigatório.");
         }
+        if(!Assert.isCnpjValido(entity.getCnpj())){
+            throw new NegocioException("CNPJ inválido.");
+        }
+        if(!Assert.isCpf(entity.getCpf())){
+             throw new NegocioException("CPF inválido.");
+        }
+         if(!Assert.isValidEmail(entity.getEmail())){
+             throw new NegocioException("Email inválido.");
+        }                  
+        
         dao.salvar(entity);
         return null;    
     }
